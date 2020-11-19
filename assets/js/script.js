44852a781d61cf110fc4cef30503970a18c43d17
postolny <74405055+postolny@users.noreply.github.com>
2020-11-20 00:59:13 +0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,11 @@
       }, 500);
       return false;
     });
-    
+
+    $("a[href='#top']").click(function() {
+      $("html, body").animate({ scrollTop: 0 }, "slow");
+      return false;
+    });
+
   });
 })(jQuery);
