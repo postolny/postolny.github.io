58e5b41de60bd653b21e2ef7a708d7356c98d860
Igor Postolny <74405055+postolny@users.noreply.github.com>
2024-02-14 20:42:39 +0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -332,15 +332,16 @@ $(function() {
         total++;
         $(".quiz-msg", this).remove();
         var correct = $(this).find(":checked[data-correct]").length;
+        var correctRadio = $(this).find("input[data-correct]");
+        var correctRadioLabel = correctRadio.parent();
         if (correct == 1) {
           var msgHTML = '<div class="quiz-msg correct">Правильно!</div>';
           $(this).append(msgHTML);
+          correctRadioLabel.removeClass("correct");
           correctAnswers++;
         } else {
           var _msgHTML = '<div class="quiz-msg incorrect">Неправильно!</div>';
           $(this).append(_msgHTML);
-          var correctRadio = $(this).find("input[data-correct]");
-          var correctRadioLabel = correctRadio.parent();
           correctRadioLabel.addClass("correct");
         }
       });
